18e0074536b191bcd4ab4bfa116727cdbafd3291
rj <rualoguh@gmail.com>
2026-05-17 23:00:21 +0000

    new format for multi-stage build

--- module/dns_zone/main.tf
+++ module/dns_zone/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_dns_zone" "main_dns_zone" {
-  name                = var.main_dns_zone_name
+  name                = var.dns_zone_name
   resource_group_name = var.resource_group_name
 }
